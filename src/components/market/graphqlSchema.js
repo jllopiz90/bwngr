a9539b04fd63ebeb3ec8daf0daa78ffdaca0b79b
jllopiz90 <jesusllopiz90@gmail.com>
2019-08-04 22:52:58 -0400

    graphql schema and resolvers for market

--- src/components/market/graphqlSchema.js
+++ src/components/market/graphqlSchema.js
@@ -1,28 +1,45 @@
 import graphqlHTTP from 'express-graphql';
-import  { buildSchema } from 'graphql';
+import { buildSchema } from 'graphql';
+import { getMarket, getManagersState } from './marketResolvers';
+require("dotenv").config();
 
 const schema = buildSchema(`
     type Query {
-        dailyMarket: [Player!]!
+        dailyMarket(league: String): [Player!]!
         managersState: [Manager!]!
     }
 
     type Player {
         name: String!
-        price: Int!
+        price: Int!,
+        price_increment: Int!
         position: String!
         team: String
+        prev_bids: [Bid]
     }
 
     type Manager {
         name: String!
-        teamValue: Int!
+        team_value: Int!
         balance: Int!
-        maxBid: Int!
-        players: [Player!]!
+        max_bid: Int!
+        team: [Player!]!
+    }
+
+    type Bid {
+        manager: String!,
+        avg_bid_overprice: Int!,
+        total_bids: Int!
     }
 `);
 
 const resolvers = {
-    
+    dailyMarket: (args) => getMarket(args.league),
+    managersState: () => getManagersState()
 }
+
+export default graphqlHTTP({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: process.env.NODE_ENV === 'dev',
+  })
