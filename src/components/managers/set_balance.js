6a396ab3063ca579c0865b9c2b160956d86234b2
jllopiz90 <jesusllopiz90@gmail.com>
2019-08-17 16:23:51 -0400

    fix set balance call

--- src/components/managers/set_balance.js
+++ src/components/managers/set_balance.js
@@ -9,12 +9,12 @@ let args = process.argv.slice(2);
 
 if(args.length > 2) {
     const [league, amount, id_bwngr] = args;
-    setBalance({amount, id_bwngr, league});
+    executeAsyncFunc(setBalance,{amount, id_bwngr, league});
 } else if(args.length > 1){
     if(isInt(args[0]) && isInt(args[1])){
         console.log('missing params, using pl by default');
         const [amount, id_bwngr] = args;
-        executeAsyncFunc(setBalance,[{amount,id_bwngr}])
+        executeAsyncFunc(setBalance,{amount,id_bwngr})
     } else {
         console.log('missing params, setting balance for all users');
         const [league,amount] = args;
@@ -24,7 +24,7 @@ if(args.length > 2) {
 } else if(args.length === 1) {
     const [amount] =args;
     console.log('missing params, using pl by default and setting balance for all users');
-    executeAsyncFunc(setBalance,[{amount}]);
+    executeAsyncFunc(setBalance,{amount});
 } else{
     console.log('missing params');
 }
