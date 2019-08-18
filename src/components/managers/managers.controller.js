8f399398c98d72d9dca48d060b7c3d02b4a0ed8a
jllopiz90 <jesusllopiz90@gmail.com>
2019-08-18 15:58:39 -0400

    call next in manager controller actions

--- src/components/managers/managers.controller.js
+++ src/components/managers/managers.controller.js
@@ -6,6 +6,7 @@ export default class ManagerController {
         try {
             const message = await ManagersDAO.getManager({}, {projection: {_id:0, name: 1, id_bwngr: 1}});
             res.json({result: true, message});
+            next();
         } catch (e) {
             res.json({result: false, message: String(e)});
         }
@@ -17,6 +18,7 @@ export default class ManagerController {
             const id_bwngr = req.params.manager;
             const message = await ManagersDAO.getManager({id_bwngr: parseInt(id_bwngr)}, {projection: {_id:0, name: 1, balance: 1}});
             res.json({result: true, message});
+            next();
         } catch (e) {
             res.json({result: false, message: String(e)});
         }
