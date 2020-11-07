0ec84fafe338f3b9922f064332d95307ab410426
jllopiz90 <jesusllopiz90@gmail.com>
2020-11-07 05:13:16 -0500

    added engines to package.json

--- src/app.js
+++ src/app.js
@@ -12,14 +12,14 @@ app.use(cors());
 process.env.NODE_ENV !== "prod" && app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use("/api/v1/:league",chooseAndInjectDB);
+app.use("/api/v1/:league",chooseAndInjectDB);
 
 app.use("/api/v1/:league/managers", managers); //########## this rout doesn't use graphql just regular express routing, is here to show how express handle routing
 app.use("/api/v1/:league/managerState", managerSchema);  // ######## Routes with graphql schemas
 app.use("/api/v1/:league/marketState", marketSchema);   // ########   
-// app.use("/api/v1/:league",closeDB);
+app.use("/api/v1/:league",closeDB);
 app.get('/', (req, res) => res.send('Hello cheater, welcome to bwngrTrack!'));
 // app.use("/", express.static("build"))   //  i don't have static resources yet
-app.use("*", (req, res) => res.json({ error: "not found" }));
+// app.use("*", (req, res) => res.json({ error: "not found" }));
 
 export default app;
